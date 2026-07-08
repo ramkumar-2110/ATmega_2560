8cad07167b14922907ccfa3c5997b2ceff759e72
Ramkumar <ramkumar21102006@gmail.com>
2026-07-08 09:08:23 +0530

    modified

--- includes/seven_segment.h
+++ includes/seven_segment.h
@@ -1,16 +1,16 @@
 #ifndef SEVEN_SEGMENT_H //To exclude multiple includes of seven_segment.h
 #define SEVEN_SEGMENT_H //Include seven_segment.h when it still not included
 
-#include "define.h" //To use seven_segment.h in main.c file must include define.h as well
-                    //To use seven_segment.h in main.c file must include gpio.h as well
+#include "define.h"
+#include "delay.h"
 
 /*****declaration of functions*****/
 
-void segment7_1(uint8_t x,uint64_t i);
-void segment7_2(uint8_t x,uint8_t y,uint64_t i);
-void segment7_3(uint8_t x,uint8_t y,uint64_t i);
-void segment7_4(uint8_t x,uint8_t y,uint64_t i);
-void segment_dot(uint8_t x,uint8_t i);
-void segment_symbol(uint8_t x,uint8_t i);
+void segment7_1(port_index_t x,uint64_t i);
+void segment7_2(port_index_t x,port_index_t y,uint64_t i);
+void segment7_3(port_index_t x,port_index_t y,uint64_t i);
+void segment7_4(port_index_t x,port_index_t y,uint64_t i);
+void segment_dot(port_index_t x,uint8_t i);
+void segment_symbol(port_index_t x,uint8_t i);
 
 #endif //end the if
